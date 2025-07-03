3ec46cf06dfbd892b5d12b6a153101ef2904a1c6
Leonel Acosta <leoacosta88@gmail.com>
2025-07-03 15:03:16 +0200

    Header - Desktop version

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,42 @@
+import Logo from "../common/Logo";
+import Button from "../ui/Button";
+
 export default function Header() {
   return (
-    <div className="text-center">
-      <h1>Header</h1>
-    </div>
+    <header className="sticky top-0 bg-secondary text-white py-4 px-10 text-center flex flex-row items-center justify-between z-50">
+      <div className="w-24" style={{ alignItems: "center" }}>
+        <a href="/#" className="hover:underline col-auto">
+          <Logo />
+        </a>
+        {/* <h1 className="text-2xl font-bold">Deportivo Latino</h1> */}
+      </div>
+      <nav className="mt-2 body-font">
+        {/* <!-- Mobile menu --> */}
+
+        {/* <!-- Desktop menu --> */}
+        <ul className="xs:hidden sm:flex sm:space-x-6 md:space-x-6 lg:space-x-8 uppercase text-sm font-semibold flex flex-row items-center justify-center">
+          <li>
+            <a href="/#mision" className="hover:underline col-auto">
+              Nuestra misión
+            </a>
+          </li>
+          <li>
+            <a href="/#teams" className="hover:underline col-auto ">
+              Equipos
+            </a>
+          </li>
+          <li>
+            <a href="/#contact" className="hover:underline col-auto">
+              Contacto
+            </a>
+          </li>
+          <li>
+            <a href="/#contact">
+              <Button accent text={"ÚNETE"} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
