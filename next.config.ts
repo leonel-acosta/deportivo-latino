3da3821b52d7ea8bed6698176a31b841d8018e64
Leonel Acosta <leoacosta88@gmail.com>
2025-07-17 16:15:12 +0200

    add cloudinary to nextJs config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+const withNextIntl = createNextIntlPlugin(nextConfig);
+
+export default withNextIntl;
