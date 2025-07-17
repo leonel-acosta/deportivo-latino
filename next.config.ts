b2e7881aa1b5a42940593f0a2d53c6526532ed37
Leonel Acosta <leoacosta88@gmail.com>
2025-07-17 17:00:24 +0200

    fix nextjs config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const withNextIntl = createNextIntlPlugin({});
 
-const withNextIntl = createNextIntlPlugin();
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
+
 export default withNextIntl(nextConfig);
