9c78d414d51a58468ad5acfe00e1ad27ada121cb
Leonel Acosta <leoacosta88@gmail.com>
2025-10-18 11:49:16 +0200

    image with text section multipurpose content refactory

--- components/sections/ImageWithText.tsx
+++ components/sections/ImageWithText.tsx
@@ -7,11 +7,14 @@ interface ImageWithTextProps {
   title: string;
   text: string;
   sectionId: string;
+  vertical?: boolean;
   reverse?: boolean;
   primary?: boolean;
   secondary?: boolean;
   tertiary?: boolean;
-  imageUrl: string;
+  center?: boolean;
+  imageUrl?: string;
+  videoUrl?: string;
   button?: string;
   buttonUrl?: string;
 }
@@ -21,10 +24,13 @@ export default function ImageWithText({
   title,
   text,
   reverse = false,
+  vertical = false,
   primary,
   secondary,
   tertiary,
+  center,
   imageUrl,
+  videoUrl,
   button,
   buttonUrl,
 }: ImageWithTextProps) {
@@ -34,19 +40,26 @@ export default function ImageWithText({
     <>
       <section
         id={sectionId}
-        className={`{"w-full grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-8 gap  lg:p-20 ${
-          primary
-            ? "bg-primary text-white"
-            : secondary
-            ? "bg-secondary text-white"
-            : tertiary
-            ? "bg-tertiary text-white"
-            : "bg-white text-foreground"
-        }`}
+        className={`w-full grid grid-cols-1 gap-2 ${
+          vertical
+            ? "lg:grid-cols-1 lg:px-20"
+            : "lg:grid-cols-2 lg:gap-8 lg:p-20"
+        }
+            ${
+              primary
+                ? "bg-primary text-white"
+                : secondary
+                ? "bg-secondary text-white"
+                : tertiary
+                ? "bg-tertiary text-white"
+                : "bg-white text-foreground"
+            }`}
       >
         <div
           className={`h-fit px-10 lg:px-20 py-20 my-auto ${
             reverse ? "order-first lg:order-last" : "order-first lg:order-first"
+          } ${
+            center ? "text-center lg:text-center" : "text-left lg:text-left"
           }`}
         >
           <h2 className="uppercase mb-2 heading-font">{title}</h2>
@@ -57,19 +70,34 @@ export default function ImageWithText({
             </Link>
           )}
         </div>
-        <div
-          className={`w-screen h-[400px] lg:w-[700px] relative overflow-hidden shadow-lg mx-auto ${
-            reverse ? "order-last lg:order-first" : "order-last lg:order-last"
-          }`}
-        >
-          <Image
-            alt="mision"
-            className="object-cover"
-            fill
-            src={imageUrl}
-            unoptimized
-          />
-        </div>
+        {(imageUrl || videoUrl) && (
+          <div
+            className={`w-full h-[400px] lg:w-full lg:h-[500px] relative overflow-hidden shadow-lg mx-auto ${
+              reverse ? "order-last lg:order-first" : "order-last lg:order-last"
+            }`}
+          >
+            {videoUrl && (
+              <video
+                src={videoUrl}
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-full h-full object-cover"
+              />
+            )}
+
+            {imageUrl && (
+              <Image
+                alt="mision"
+                className="object-cover"
+                fill
+                src={imageUrl}
+                unoptimized
+              />
+            )}
+          </div>
+        )}
       </section>
     </>
   );
